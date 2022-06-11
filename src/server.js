6232b99f3bcde30f0e593eab2daeddf8507aa71e
Carlos Matos <lematosdev@gmail.com>
2022-06-11 19:29:15 -0400

    first commit

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import cors from 'cors';
+import express from 'express';
+import routes from './routes/products.js';
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+//Settings
+app.set(PORT);
+
+//Middlewares
+app.use(express.json());
+
+//Routes
+app.use(routes);
+
+//Starting the Server
+
+app.listen(PORT, () => {
+  console.log(`Server connected on port ${PORT}`);
+});
